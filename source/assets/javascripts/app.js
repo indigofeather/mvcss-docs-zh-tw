9f467e14b74114dfae889aa607bd12b4ef89e718
Lance <indigofeather@gmail.com>
2014-08-13 08:50:35 +0800

    增加原文連結

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,3 +1,4 @@
-var git = 'c6ea164933081a160fe327c06f15484d5b6aaeb8';
+var git = 'c6ea164933081a160fe327c06f15484d5b6aaeb8',
+  path = window.location.pathname;
 
-$('aside.sidebar').append('<span><a class="lance" href="http://blog.ycnets.com">@LanceHe</a></span></span><span><a href="https://github.com/mvcss/mvcss.github.com/commit/'+git+'">'+git.substr(0, 7)+'</a></span>');
+$('aside.sidebar').append('<span><a class="lance" href="http://blog.ycnets.com">@LanceHe</a></span></span><span><a href="https://github.com/mvcss/mvcss.github.com/commit/'+git+'">'+git.substr(0, 7)+'</a></span><span><a href="http://mvcss.github.io'+path+'">原文</a></span>');
