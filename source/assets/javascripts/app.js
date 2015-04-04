eaafce18c92955b2ed95be8fbd0cb5353a9efae4
Lance <indigofeather@gmail.com>
2015-04-04 17:09:04 +0800

    156170dd4a862b6d9e648de133358c9551f18abb

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-var git = '914d12e973ab7220ee376db9bd997fa2b32990e7',
+var git = '156170dd4a862b6d9e648de133358c9551f18abb',
   path = window.location.pathname;
 
 $('aside.sidebar').append('<span><a class="lance" href="http://blog.ycnets.com">@LanceHe</a></span></span><span><a href="https://github.com/mvcss/mvcss.github.com/commit/'+git+'">'+git.substr(0, 7)+'</a></span><span><a href="http://mvcss.io'+path+'">原文</a></span>');
